329aa14bf4476ab456c30c6487f26619d7bb139b
jasko09 <jankaleta9@gmail.com>
2021-06-01 20:20:31 +0200

    test

---
+++ main.js
@@ -0,0 +1,32 @@
+class Produkt {
+    constructor(nazwa, kolor, rozmiar, cena) {
+        this.nazwa = nazwa;
+        this.kolor = kolor;
+        this.rozmiar = rozmiar;
+        this.cena = cena;
+        this.wybierz_rozmiar = function (){
+
+        document.getElementById('rozmiar').innerHTML = "Wybierz rozmiar: ";
+        
+            
+         }
+         this.wybierz_kolor = function(){
+
+         }
+         this.wyszukaj_produkty = function(){
+
+         }
+    }
+}
+
+
+test = function(){
+   produkt1 = new Produkt('koszulka','czarny');
+   produkt1.wybierz_rozmiar();
+
+}
+
+//test();
+
+
+
