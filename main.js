b7638ac48a396f99df22a35a88dcf49f1cc09db2
Dorian Bachlaj <bachlaj.dorian@gmail.com>
2021-06-10 01:11:18 +0200

    konfiguracja produktu

--- main.js
+++ main.js
@@ -1,32 +1,71 @@
+class Koszyk {
+    constructor(listaProduktow) {
+        this.listaProduktow = listaProduktow;
+
+    }
+}
+
 class Produkt {
     constructor(nazwa, kolor, rozmiar, cena) {
         this.nazwa = nazwa;
         this.kolor = kolor;
         this.rozmiar = rozmiar;
-        this.cena = cena;
-        this.wybierz_rozmiar = function (){
-
-        document.getElementById('rozmiar').innerHTML = "Wybierz rozmiar: ";
-        
-            
-         }
-         this.wybierz_kolor = function(){
-
-         }
-         this.wyszukaj_produkty = function(){
-
-         }
+        this.cena = cena;        
+         
     }
 }
 
 
-test = function(){
-   produkt1 = new Produkt('koszulka','czarny');
-   produkt1.wybierz_rozmiar();
+wyszukaj_produkt = function(){
+    produkt = new Produkt();
 
+
+    if(document.getElementById('koszulka').checked) {
+        produkt.nazwa = "koszulka";
+        produkt.cena = 50;
+    }else if(document.getElementById('spodnie').checked) {
+        produkt.nazwa = "spodnie";
+        produkt.cena = 100;
+    }else{
+        produkt.nazwa = "skarpetki";
+        produkt.cena = 20;
+    }
+
+    if(document.getElementById('czerwony').checked) {
+        produkt.kolor = "czerwony";
+        produkt.cena += 5;
+    }else if(document.getElementById('niebieski').checked) {
+        produkt.kolor = "niebieski";
+        produkt.cena += 2;
+    }else{
+        produkt.kolor = "zielony";
+        produkt.cena -= 3;
+    }
+
+    if(document.getElementById('large').checked) {
+        produkt.rozmiar = "L";
+        produkt.cena *= 1.02;
+    }else if(document.getElementById('medium').checked) {
+        produkt.rozmiar = "M";
+    }else{
+        produkt.rozmiar = "S";
+        produkt.cena *= 1.05;
+    }
+    
+    let h1 = document.createElement("h1");
+
+    //setting this css style solving problem with new line in textContent
+    h1.setAttribute('style', 'white-space: pre;');
+
+    document.getElementById('wyszukany_produkt').textContent = produkt.nazwa + ", rozmiar: " + produkt.rozmiar + ", kolor: ";
+    document.getElementById('wyszukany_produkt').textContent += produkt.kolor + "-- CENA: " + produkt.cena + " zł";
+
+    console.log(produkt.nazwa);
+    console.log(produkt.kolor);
+    console.log(produkt.rozmiar);
+    console.log(produkt.cena);
+ }
+
+main = function() {
+    koszyk = new Koszyk();
 }
-
-//test();
-
-
-
